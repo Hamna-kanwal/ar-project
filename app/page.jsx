15a566cb209a5190df4a6e28aa8b9d778e82bf44
Hamna Kanwal <hamnakanwal725@gmail.com>
2026-06-25 14:55:31 +0500

    Final fix for component imports

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import { HeroSlider } from "./Components/HeroSlider";
 import { ServiceList } from "./Components/ServiceList";
 import { WhyChooseUs } from "./Components/Strategy";
 import { AboutSection } from "./Components/AboutSection";
-import { Cta } from "./Components/Cta";
+import { CTA } from "./Components/Cta";
 
 export default function Home() {
   return (

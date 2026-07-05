293f2dd3eed3bf8d86ffac9ba68a97da309bfe9a
Hamna Kanwal <hamnakanwal725@gmail.com>
2026-07-05 14:54:39 +0500

    updated

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -9,22 +9,24 @@ export default function Navbar({ onOpenSidebar }) {
     <>
       {/* Top Contact Bar */}
       {/* Padding px-14 se badal kar px-12 kr di */}
-      <div className="relative  bg-[#027cc1] text-white py-6 px-14 flex items-center justify-between text-xs border-b border-white/10">
-        <div className="flex items-center gap-6">
-          <a href="mailto:info@arheatingservice.co.uk" className="flex items-center gap-2 hover:text-gray-200">
-            <Mail size={22} /> info@arheatingservice.co.uk
-          </a>
-          <a href="tel:+447800657141" className="flex items-center gap-2 hover:text-gray-200">
-            <Phone size={22} /> +447800657141
-          </a>
-        </div>
+     <div className="bg-[#027cc1] text-white py-4 px-4 md:px-14 flex flex-wrap items-center justify-center md:justify-between gap-y-3 text-[11px] md:text-xs border-b border-white/10">
+  
+  {/* Left Section - Contact Info */}
+  <div className="flex flex-wrap items-center justify-center gap-4 md:gap-6">
+    <a href="mailto:info@arheatingservice.co.uk" className="flex items-center gap-2 hover:text-gray-200">
+      <Mail size={16} /> info@arheatingservice.co.uk
+    </a>
+    <a href="tel:+447800657141" className="flex items-center gap-2 hover:text-gray-200">
+      <Phone size={16} /> +447800657141
+    </a>
+  </div>
 
-        <div className="flex items-center gap-4">
-         
-          <a href="#" className="hover:text-gray-200"><FaFacebook size={22} /></a>
-          <a href="#" className="hover:text-gray-200"><FaInstagram size={22} /></a>
-        </div>
-      </div>
+  {/* Right Section - Social Media */}
+  <div className="flex items-center gap-4">
+    <a href="#" className="hover:text-gray-200"><FaFacebook size={18} /></a>
+    <a href="#" className="hover:text-gray-200"><FaInstagram size={18} /></a>
+  </div>
+</div>
 
     
       <header className="absolute top-6  left-0 w-full z-50 bg-transparent">

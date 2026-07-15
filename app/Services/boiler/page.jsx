bd288ee81a210eaba9b465cf175789cbe7e02634
Hamna Kanwal <hamnakanwal725@gmail.com>
2026-07-15 17:09:17 +0500

    updated

--- app/Services/boiler/page.jsx
+++ app/Services/boiler/page.jsx
@@ -32,7 +32,7 @@ export default function ServicesAndFAQ() {
       {/* Hero Section */}
       <section className="relative w-full h-[700px] flex items-center justify-center overflow-hidden">
         <NextImage
-          src="/boiler.jpg"faqs
+          src="/boiler.jpg"
           alt="Boiler service background"
           fill
           priority
@@ -40,10 +40,9 @@ export default function ServicesAndFAQ() {
           quality={70}
           className="object-cover"
         />
-        <div className="absolute inset-0 bg-white/80" />
-        <div className="relative z-10 text-center px-6 max-w-4xl pt-28 ">
+          <div className="relative z-10 text-center px-6 max-w-4xl">
           <h1 className="text-5xl md:text-6xl font-bold mb-6 tracking-tight text-[#027cc1]">
-            Boiler <span className="text-orange-500"> Services</span>
+            Boiler <span className="text-orange-500">services </span>
           </h1>
           <p className="text-lg text-gray-800 max-w-2xl mx-auto mt-6 font-medium">
             A yearly boiler check keeps your home safe and your boiler running well. A small spend now saves a big bill later. 

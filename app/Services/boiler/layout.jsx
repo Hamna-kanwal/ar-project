1ed1abb94ae6fdd494aa7c9f0cefa08801ccc8f8
Hamna Kanwal <hamnakanwal725@gmail.com>
2026-07-16 18:51:51 +0500

    meta title

---
+++ app/Services/boiler/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: " New Boiler Installation Watford | AR Heating & Plumbing",
+  description: " Gas Safe new boiler fits in Watford. Most take one day. Free fixed quotes and honest advice. Call AR Heating today",
+};
+
+export default function BoilerLayout({ children }) {
+  return <>{children}</>;
+}

e30aa6557731e62168a7b5f34d061458466d3a66
Hamna Kanwal <hamnakanwal725@gmail.com>
2026-09-22 19:18:12 +0500

    redirection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/Services/:slug*',
+        destination: '/services/:slug*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
